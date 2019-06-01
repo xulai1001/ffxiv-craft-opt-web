cee565a1a8c036aebc46baa7e665dcef1708b022
viktorxu <xulai1001@gmail.com>
2019-06-01 16:18:06 +0000

    June 2, 2019 backup mirror

--- app/js/ffxivcraftmodel.js
+++ app/js/ffxivcraftmodel.js
@@ -2017,7 +2017,7 @@ var Ing1RecipeLevelTable = {
     300: 291,   // 70_1star
     320: 292,   // 70_2star
     350: 293,   // 70_3star
-	390: 294   // 70_4star
+	380: 294   // 70_4star
 };
 
 var Ing2RecipeLevelTable = {
@@ -2066,7 +2066,7 @@ var Ing2RecipeLevelTable = {
     300: 290,   // 70_1star
     320: 291,   // 70_2star
     350: 292,   // 70_3star
-	390: 293   // 70_4star
+	380: 293   // 70_4star
 };
 
 var NymeaisWheelTable = {
@@ -2090,7 +2090,7 @@ var ProgressPenaltyTable = {
     250: -0.04,
     320: -0.02,
     350: -0.035,
-	390: -0.0465   // 70_4star
+	380: -0.0465   // 70_4star
 };
 
 var QualityPenaltyTable = {
@@ -2103,7 +2103,7 @@ var QualityPenaltyTable = {
     300: -0.09,
     310: -0.10,
     340: -0.11,
-	390: -0.1305
+	380: -0.1305
 };
 
 // Test objects
